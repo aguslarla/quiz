4b3ba58c9a9dc024b58bfe2899534bf1b3d50c92
agustin <arivillasollero@yahoo.es>
2015-07-16 12:32:00 +0200

    Revert "version 2.0"
    This reverts commit 619f5d178d1f9f9c04161514403fbf05e43f85c6.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
+var users = require('./routes/users');
 
 var app = express();
 
@@ -22,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -54,5 +56,5 @@ app.use(function(err, req, res, next) {
   });
 });
 
-// Exporta app para comando de arranque
+
 module.exports = app;
