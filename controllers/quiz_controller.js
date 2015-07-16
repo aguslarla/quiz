72b7fb6122f00846d116ad99b9ee9702d37e7bd9
agustin <arivillasollero@yahoo.es>
2015-07-16 15:01:55 +0200

    versión 1.2
    Versión  buena en la que sale la primera pregunta.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,12 +1,15 @@
 // GET quizes/question
 exports.question = function(req, res){
-	res.render('quizes/question', {pregunta: "Capital de Italia"});
+	res.render('quizes/question', {pregunta: 'Capital de Italia'});
 }
 
 // GET quizes/answer
 exports.answer = function(req, res){
 	if (req.query.respuesta === 'Roma')
-		res.render('quizes/answer', {respuesta: "Correcto"});
+		res.render('quizes/answer', {respuesta: 'Correcta!!'});
 	else
-		res.render('quizes/answer', {respuesta: "Correcto"});
+	{
+		res.render('quizes/answer', {respuesta: 'Incorrecta!!'});
+		res.render('quizes/answer', {color_resp: 'Red'});		
+	}
 }
