76b3be526c07279d60e303c55a68da4919b9ee23
agustin <arivillasollero@yahoo.es>
2015-07-23 10:53:17 +0200

    versión 1.4 Lista de preguntas
    Se hace un commit actualizando el código hasta el tema 4, incluido, del
    módulo 7.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,15 +1,25 @@
-// GET quizes/question
-exports.question = function(req, res){
-	res.render('quizes/question', {pregunta: 'Capital de Italia'});
-}
+var models = require('../models/models.js');
+
+// GET /quizes
+exports.index = function(req, res){
+	models.Quiz.findAll().then(function(quizes){
+		res.render('quizes/index.ejs', {quizes: quizes});
+	})
+};
+
+// GET quizes/show
+exports.show = function(req, res){
+	models.Quiz.find(req.params.quizId).then function(quiz){
+		res.render('quizes/show', {quiz: quiz});
+	})
+};
 
 // GET quizes/answer
 exports.answer = function(req, res){
-	if (req.query.respuesta === 'Roma')
-		res.render('quizes/answer', {respuesta: 'Correcta!!'});
-	else
-	{
-		res.render('quizes/answer', {respuesta: 'Incorrecta!!'});
-		res.render('quizes/answer', {color_resp: 'Red'});		
-	}
-}
+	models.Quiz.find(req.params.quizId).then function(quiz){
+		if (req.query.respuesta === quiz.respuesta)
+			res.render('quizes/answer', {respuesta: 'Correcta!!'});
+		else
+			res.render('quizes/answer', {respuesta: 'Incorrecta!!'});
+	})
+};
---
+++ models/models.js
@@ -0,0 +1,44 @@
+var path = require('path');
+
+// Postgres DATABASE_URL 	= postgres:/user:passwd@host:port/database
+// SQLite DATABASE_URL		= sqlite://:@:/
+var url= process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name	= 	(url[6]||null);
+var user	=	(url[2]||null);
+var pwd		=	(url[3]||null);
+var protocol=	(url[1]||null);
+var dialect	=	(url[1]||null);
+var port	=	(url[5]||null);
+var host	=	(url[4]||null);
+var storage	=	process.env.DATABASE_STORAGE;
+
+// Carga modelo ORM
+var Sequelize = require('sequelize');
+
+// Usar BD Sqlite o Postgres
+var sequelize = new Sequelize(DB_name, user, pwd, {dialect : dialect,
+													,protocol: protocol,
+													,port: port,
+													,host: host,
+													,storage: storage, // sólo SQLite (.env)
+													,omitNull: true}); // sólo Postgres
+
+
+
+// Importar la definición de la tabla Quiz en 'quiz.js'
+var Quiz = Sequelize.import(path.join(__dirname, 'quiz'));
+exports.Quiz = Quiz;
+
+// sequelize.sync crea e inicializa la tabla, en este caso de "preguntas", en la BD.
+sequelize.sync().success(function(){
+	//success ejecuta el manejador una vez creada
+	Quiz.count().success(function (count){
+		// Se inicializa sólo si está vacía
+		if (count === 0){
+			Quiz.create({pregunta: 'Capital de Italia', 
+						respuesta: 'Roma'});
+			 Quiz.create({pregunta: 'Capital de Portugal', 
+						respuesta: 'Lisboa'});
+		};
+	});
+});
