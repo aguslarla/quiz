2fdce74c69f44501f42d0ab783833d52087effd8
aguslarla <arivillasollero@yahoo.es>
2015-08-07 14:06:44 +0200

    versión 3.0 Despliegue Heroku
    Aún no funciona.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -53,16 +53,19 @@ exports.index = function(req, res){
 };
 
 // GET quizes/show
-exports.show = function(req, res){
-	//models.Quiz.find(req.params.quizId).then(function(quiz){
-		res.render('quizes/show', {quiz: req.quiz})	
+exports.question = function(req, res){
+	models.Quiz.findAll().success(function(quiz){
+		res.render('quizes/question', {pregunta: quiz[0].pregunta})})	
 };
 
 // GET quizes/answer
 exports.answer = function(req, res){
-	var resultado = "Incorrecto";
-	if (req.query.respuesta === req.quiz.respuesta)
-		resultado = "Correcto";
+	models.Quiz.findAll().success(function(quiz){
+		if (req.query.respuesta === quiz[0].respuesta)
+			res.render('quizes/answer', {respuesta: "Correcto"});
+		else
+			res.render('quizes/answer', {respuesta: "Incorrecto"});
+	})
 	
-	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+	
 };
