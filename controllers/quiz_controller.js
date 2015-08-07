27eb676447882a1a597584ea393dfaa96d902049
aguslarla <arivillasollero@yahoo.es>
2015-08-07 12:05:34 +0200

    Versión 2.0 Modulo 7
    Sin el ejercicio obligatorio.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,7 +1,7 @@
 var models = require('../models/models.js');
 
 // Para que haga el AutoLoad
-exports.load = function(req, res, next, quizId){
+/*exports.load = function(req, res, next, quizId){
 	models.Quiz.find(quizId).then(function (quiz){
 		if (quiz){
 			req.quiz = quiz;
@@ -22,6 +22,41 @@ exports.index = function(req, res){
 // GET quizes/show
 exports.show = function(req, res){
 	res.render('quizes/show', {quiz: req.quiz});	
+};
+
+// GET quizes/answer
+exports.answer = function(req, res){
+	var resultado = "Incorrecto";
+	if (req.query.respuesta === req.quiz.respuesta)
+		resultado = "Correcto";
+	
+	res.render('quizes/answer', {quiz: req.quiz, respuesta: resultado});
+};
+*/
+
+// Para que haga el AutoLoad
+exports.load = function(req, res, next, quizId){
+	models.Quiz.find(quizId).then(function (quiz){
+		if (quiz){
+			req.quiz = quiz;
+			next();
+		}	
+		else
+			next(new Error('No existe quizId ' + quizId));
+	}).catch(function(error){next(error);});
+};
+
+// GET /quizes
+exports.index = function(req, res){
+	models.Quiz.findAll().then(function(quiz){
+		res.render('quizes/index.ejs', {quizes:quizes})})
+};
+
+// GET quizes/show
+exports.show = function(req, res){
+	models.Quiz.find(req.params.quizId).then(function(quiz){
+		res.render('quizes/show', {quiz: req.quiz});	
+	})	
 };
 
 // GET quizes/answer
--- models/models.js
+++ models/models.js
@@ -1,44 +1,61 @@
 var path = require('path');
 
 // Postgres DATABASE_URL 	= postgres:/user:passwd@host:port/database
+// Variable DATABASE_URL en HEROKU ---> postgres://esjqhcdsxqtkfv:ZqMDnGeas-6892wrpl-JFXhwez@ec2-54-217-202-108.eu-west-1.compute.amazonaws.com:5432/drtgkmjqh71so
 // SQLite DATABASE_URL		= sqlite://:@:/
-var url= process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
-var DB_name	= 	(url[6]||null);
+//var url= process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+/*var DB_name	= 	(url[6]|null);
 var user	=	(url[2]||null);
 var pwd		=	(url[3]||null);
 var protocol=	(url[1]||null);
 var dialect	=	(url[1]||null);
 var port	=	(url[5]||null);
 var host	=	(url[4]||null);
-var storage	=	process.env.DATABASE_STORAGE;
+
+/*var DB_name	= 	"drtgkmjqh71so";				 
+var user	=	"esjqhcdsxqtkfv";				 
+var pwd		=	"ZqMDnGeas-6892wrpl-JFXhwez";				 
+var protocol=	"postgres";
+var dialect	=	"postgres";
+var port	=	5432;
+var host	=	"ec2-54-217-202-108.eu-west-1.compute.amazonaws.com";
+	*/			 
+
+var storage	=	"quiz.sqlite";
+console.log("models.js storage "+storage);
 
 // Carga modelo ORM
 var Sequelize = require('sequelize');
 
 // Usar BD Sqlite o Postgres
-var sequelize = new Sequelize(DB_name, user, pwd, {dialect : dialect,
-													,protocol: protocol,
-													,port: port,
-													,host: host,
-													,storage: storage, // sólo SQLite (.env)
-													,omitNull: true}); // sólo Postgres
-
+/*var sequelize = new Sequelize(DB_name, user, pwd, {dialect : dialect,
+													protocol: protocol,
+													port: port,
+													host: host,
+													storage: storage, // sólo SQLite (.env)
+													omitNull: true}); // sólo Postgres
+*/
+var sequelize = new Sequelize(null, null, null, {dialect : "sqlite",
+							storage: "quiz.sqlite"}); // sólo Postgres
 
 
 // Importar la definición de la tabla Quiz en 'quiz.js'
-var Quiz = Sequelize.import(path.join(__dirname, 'quiz'));
+var quiz_path = path.join(__dirname, 'quiz');
+var Quiz = sequelize.import(quiz_path);
 exports.Quiz = Quiz;
 
+console.log("models.js Aqui llega 1");
 // sequelize.sync crea e inicializa la tabla, en este caso de "preguntas", en la BD.
 sequelize.sync().success(function(){
+	console.log("models.js Aqui llega 2");
 	//success ejecuta el manejador una vez creada
 	Quiz.count().success(function (count){
 		// Se inicializa sólo si está vacía
 		if (count === 0){
 			Quiz.create({pregunta: 'Capital de Italia', 
-						respuesta: 'Roma'});
-			 Quiz.create({pregunta: 'Capital de Portugal', 
-						respuesta: 'Lisboa'});
+						respuesta: 'Roma'}).success(function(){console.log("base de datos inicializada")});
+			//Quiz.create({pregunta: 'Capital de Portugal', 
+			//			respuesta: 'Lisboa'});
 		};
 	});
 });
